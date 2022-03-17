9bc263b4c2928296e0799d6f7730c781043171f1
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-17 20:10:54 +0100

    Usage of the command pattern on MainActivity

--- app/src/main/java/com/eps/intentsimplicits/MainActivity.kt
+++ app/src/main/java/com/eps/intentsimplicits/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.eps.intentsimplicits
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.eps.intentsimplicits.databinding.ActivityMainBinding
+import com.eps.intentsimplicits.intents.Command
 import com.eps.intentsimplicits.intents.types.Caller
 import com.eps.intentsimplicits.intents.types.ContactsOpener
 import com.eps.intentsimplicits.intents.types.LocateAddress
 import com.eps.intentsimplicits.intents.types.LocateCoordinates
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -16,26 +18,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setUpButtonComponents()
     }
 
-    fun setUpButtonComponents() {
-        binding.callButton.setOnClickListener {
-            val caller = Caller(this)
-            caller.execute()
-        }
-        binding.contactsButton.setOnClickListener {
-            val contacts = ContactsOpener(this)
-            contacts.execute()
-        }
-        binding.coordinatesButton.setOnClickListener {
-            val locate = LocateCoordinates(this)
-            locate.execute()
-        }
-        binding.directionButton.setOnClickListener {
-            val locateAddress = LocateAddress(this)
-            locateAddress.execute()
-        }
+    fun getCommandFromButton(buttonId: Int): Command = when (buttonId) {
+        binding.callButton.id -> Caller(this)
+        binding.contactsButton.id -> ContactsOpener(this)
+        binding.coordinatesButton.id -> LocateCoordinates(this)
+        binding.directionButton.id -> LocateAddress(this)
+        else -> throw IllegalArgumentException("Unsupported button")
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0 != null)
+            getCommandFromButton(p0.id).execute()
     }
 
 }
